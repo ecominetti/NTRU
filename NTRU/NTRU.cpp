5eee389a428a4af4cbba1cec9a475d98056d21db
ecominetti <ecominetti@gmail.com>
2016-03-03 19:49:14 -0300

    teste de funcionamento de multpoly

--- NTRU/NTRU.cpp
+++ NTRU/NTRU.cpp
@@ -7,12 +7,14 @@
 int main()
 {
 	int a[5] = { 1,2,3,4,5 };
-	int b[4] = { 6,7,8,9 };
+	int b[5] = { 6,7,8,9,10 };
 	int c[5];
 
 	printf("Testando novamente funcionamento da chave oath");
 	sumPoly(a, b, c, sizeof(a) / sizeof(int), sizeof(b) / sizeof(int));
-
+	multPoly(a, b, c, sizeof(a) / sizeof(int), sizeof(b) / sizeof(int));
+	for (int i = 0; i < 5; i++)
+		printf("%d ", c[i]);
 	system("PAUSE");
     return 0;
 }
