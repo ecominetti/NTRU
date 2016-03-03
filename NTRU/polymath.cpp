542853bb336237969a464e1df5d6afcacb051a82
Eduardo Cominetti <ecominetti@gmail.com>
2016-03-03 16:45:02 -0300

    adding mult function

--- NTRU/polymath.cpp
+++ NTRU/polymath.cpp
@@ -4,7 +4,7 @@ void sumPoly(int vector1[], int *vector2, int *result, int size1, int size2)
 {
 	if (size1 == size2)
 	{
-		for (int i = 0; i <= sizeof(vector1); i++)
+		for (int i = 0; i < size1; i++)
 		{
 			result[i] = vector1[i] + vector2[i];
 		}
@@ -15,5 +15,26 @@ void sumPoly(int vector1[], int *vector2, int *result, int size1, int size2)
 
 void multPoly(int* vector1, int* vector2, int* result, int size1, int size2)
 {
-
+	int j;
+	if (size1 == size2)
+	{
+		for (int i = 0; i < size1; i++)
+		{
+			result[i] = 0;
+		}
+		for (int k = 0; k < size1; k++)
+		{
+			for (int i = 0; i <= k; i++)
+			{
+				result[k] += vector1[i]*vector2[k - i];
+			}
+			for (int i = k+1; i < size1; i++)
+			{
+				j = (size1 + (k + 1) - i)-1;
+				result[k] += vector1[i] * vector2[j];
+			}
+		}
+	}
+	else
+		printf("\nVetores de diferentes tamanhos\n");
 }
